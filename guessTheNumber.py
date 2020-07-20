4a98f90c0f5e0027023a5b053bd40ef7044d22ec
ariso353 <MrsAmanda.Riso@gmail.com>
2020-07-19 17:20:00 -0700

    Updated scripts per sonarlint

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -5,10 +5,10 @@ Author: Amanda Riso
 ******History******
 Date      Initials  Description of change
 --------  --------  -----------------------------------------------------------
-20180816  ANR       Changed isValidGuess to include .isdigit() check.
+20180816  ANR       Changed is_valid_guess to include .isdigit() check.
                     Added variables to for beginning and end of range and
                     modified the prompt to use the new variables. Changed
-                    invalid guess print. Removed checking guess to a checkGuess
+                    invalid guess print. Removed checking guess to a check_guess
                     method. Added Comments.
 20180814  ANR       Initial Script
 '''
@@ -21,29 +21,29 @@ endRng = 20
 
 
 # Generates random number between given range.
-def generateRandomNumber():
+def generate_random_number():
     return random.randint(strtRng, endRng)
 
 
 # Ask the user to guess a number between a range.
-def promptUser():
+def prompt_user():
     return input("Guess a number between {} and {} :".format(strtRng, endRng))
 
 
 # Check if the guess entered is a number and if it is between the range.
-def isValidGuess(userInput):
-    if userInput.isdigit() and strtRng <= int(userInput) <= endRng:
+def is_valid_guess(user_input):
+    if user_input.isdigit() and strtRng <= int(user_input) <= endRng:
         return True
     else:
         return False
 
 
 # Checks if the user's guess is correct or not
-def checkGuess(guess, randomNumber):
-    if guess == randomNumber:
+def check_guess(guess, random_number):
+    if guess == random_number:
         print("You guessed right!")
         return False
-    elif guess > randomNumber:
+    elif guess > random_number:
         print("Your guess was too high!")
         return True
     else:
@@ -52,17 +52,17 @@ def checkGuess(guess, randomNumber):
 
 
 def main():
-    randomNumber = generateRandomNumber()
-    promptAgain = True
+    random_number = generate_random_number()
+    prompt_again = True
 
-    while(promptAgain):
-        userGuess = promptUser()
+    while(prompt_again):
+        user_guess = prompt_user()
 
-        if isValidGuess(userGuess):
-            promptAgain = checkGuess(int(userGuess), randomNumber)
+        if is_valid_guess(user_guess):
+            prompt_again = check_guess(int(user_guess), random_number)
         else:
             print("That was not a valid entry. Try again")
-            promptAgain = True
+            prompt_again = True
 
 
 if __name__ == "__main__":
